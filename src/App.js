f5b1c3d3b70f4aa3e621bbcc85e201e2009d38db
Aayush Shrestha <be2020s698@gces.edu.np>
2025-06-20 22:02:05 +0545

    Resolved stash conflicts

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Products from "./Products";
+import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const theme = {
+    colors: {
+      heading: "rgb(24 24 29)",
+      text: "rgba(29 ,29, 29, .8)",
+      white: "#fff",
+      black: " #212529",
+      helper: "#8490ff",
+
+      bg: "#f2f2f2",
+      footer_bg: "rgb(39, 51, 51)",
+      btn: "rgb(44, 55, 61)",
+      border: "rgba(54, 54, 54, 0.5)",
+      hr: "#ffffff",
+      gradient:
+        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      shadow:
+        "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+      shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+    },
+    media: {
+      mobile: "768px",
+      tab: "998px",
+    },
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import HeroSection from "./components/HeroSection";
+import ImageSearch from "./components/ImageSearch";
+import AboutSection from "./components/AboutSection";
+import RecommendedSection from "./components/RecommendedSection";
+import FloatingSearchButton from "./components/FloatingSearchButton";
+
+const Home = () => {
+  const data = {
+    name: "VisuoFind",
+  };
+
+  const searchRef = useRef(null);
+
+  const scrollToSearch = () => {
+    searchRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      <div ref={searchRef} id="search-section">
+        {" "}
+        {/* ref is like a bookmark on a part of the page. */}
+        <ImageSearch />
+      </div>
+      <RecommendedSection />
+      <AboutSection />
+      <FloatingSearchButton onSearchClick={scrollToSearch} />{" "}
+      {/*onSearchClick is passed into the FloatingSearchButton section as prop, so in there, scrollToSearch will be used/called using onSearchClick.*/}
+    </>
+  );
+};
+
+export default Home;
